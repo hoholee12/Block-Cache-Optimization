06b9cd210e5186f8f4501023f1cfb1913ff84c8d
hoholee12 <hoholee12@gmail.com>
2022-08-31 01:00:16 +0900

    Label

--- cache/sharded_cache.cc
+++ cache/sharded_cache.cc
@@ -17,23 +17,24 @@
 #include "util/math.h"
 #include "util/mutexlock.h"
 
-
+//////////////////
+// benchmark stuff
 time_t shardtotaltime[SHARDCOUNT];
 uint64_t shardaccesscount[SHARDCOUNT];
 time_t readtotaltime[SHARDCOUNT];
 uint64_t numshardbits;
 uint64_t shardnumlimit;
-
-
 uint32_t threadnumshard[SHARDCOUNT];
-
 bool enableshardfix;
 uint64_t shardsperthread;
+//////////////////
 
+//////////////////////////////
+// internals counters for CBHT
 int N = 0;
 int called = 0;
 uint64_t shardaccesscount_internal[SHARDCOUNT];
-
+//////////////////////////////
 
 namespace ROCKSDB_NAMESPACE {
 
