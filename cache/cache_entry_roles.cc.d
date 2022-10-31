4be03cc517f356be25bf3ae4f0b874f5b69bc066
hoholee12 <hoholee12@gmail.com>
2022-10-31 18:25:26 +0900

    Untrack binaries

--- cache/cache_entry_roles.cc.d
+++
@@ -1,7 +0,0 @@
-cache/cache_entry_roles.cc.d cache/cache_entry_roles.o \
- cache/cache_entry_roles.o: cache/cache_entry_roles.cc \
- cache/cache_entry_roles.h include/rocksdb/cache.h \
- include/rocksdb/memory_allocator.h include/rocksdb/status.h \
- include/rocksdb/slice.h include/rocksdb/cleanable.h \
- include/rocksdb/rocksdb_namespace.h include/rocksdb/statistics.h \
- port/lang.h
--- db/compaction/sst_partitioner.cc.d
+++
@@ -1,4 +0,0 @@
-db/compaction/sst_partitioner.cc.d db/compaction/sst_partitioner.o \
- db/compaction/sst_partitioner.o: db/compaction/sst_partitioner.cc \
- include/rocksdb/sst_partitioner.h include/rocksdb/rocksdb_namespace.h \
- include/rocksdb/slice.h include/rocksdb/cleanable.h
